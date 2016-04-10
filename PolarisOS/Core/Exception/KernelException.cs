36e53a01b1a489e350ee2720b041cafc37bd1438
Yuuki Wesp <ls-micro@ya.ru>
2016-04-10 21:01:09 +0300

    add Kernel Exception

---
+++ PolarisOS/Core/Exception/KernelException.cs
@@ -0,0 +1,15 @@
+﻿namespace PolarisOS.Core.Exception
+{
+    public class KernelException : System.Exception
+    {
+        public string MethodBase { private set; get; }
+        public ulong UID { private set; get; } = 289275347456;
+
+        public KernelException(string msg) : base(msg) { }
+
+        public KernelException(string msg, string method) : this(msg)
+        {
+            MethodBase = method;
+        }
+    }
+}
