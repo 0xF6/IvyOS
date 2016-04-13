78e1ae1e9fddbc26f0c39f2439ca390de750e7e7
Yuuki Wesp <ls-micro@ya.ru>
2016-04-13 17:42:39 +0300

    Add audio driver and etc drive

---
+++ PolarisOS/Driver/Core/IRQ.cs
@@ -0,0 +1,39 @@
+﻿namespace PolarisOS.Driver.Core
+{
+    public class IRQ
+    {
+        public static void SetMask(byte IRQline)
+        {
+            ushort port;
+
+            if (IRQline < 8)
+            {
+                port = 0x20 + 1;
+            }
+            else
+            {
+                port = 0xA0 + 1;
+                IRQline -= 8;
+            }
+            var value = (byte)(IO.IOPort.inb(port) | (1 << IRQline));
+            IO.IOPort.outb(port, value);
+        }
+        public static void ClearMask(byte IRQline)
+        {
+            ushort port;
+
+            if (IRQline < 8)
+            {
+                port = 0x20 + 1;
+            }
+            else
+            {
+                port = 0xA0 + 1;
+                IRQline -= 8;
+            }
+            var value = (byte)(IO.IOPort.inb(port) & ~(1 << IRQline));
+            IO.IOPort.outb(port, value);
+        }
+    }
+}
+}
---
+++ PolarisOS/Driver/Core/PIT.cs
@@ -0,0 +1,68 @@
+﻿namespace PolarisOS.Driver.Core
+{
+    public class PIT
+    {
+        public static void Mode0(uint frequency)
+        {
+            IDT.Remap();
+            IRQ.ClearMask(0);
+            IRQ.ClearMask(15);
+            uint divisor = 1193180 / frequency;
+            IO.IOPort.outb(0x43, 0x30);
+            IO.IOPort.outb(0x40, (byte)(divisor & 0xFF));
+            IO.IOPort.outb(0x40, (byte)((divisor >> 8) & 0xFF));
+        }
+        public static void Mode2(uint frequency)
+        {
+            IDT.Remap();
+            IRQ.ClearMask(0);
+            IRQ.ClearMask(15);
+            uint divisor = 1193180 / frequency;
+            IO.IOPort.outb(0x43, 0x36);
+            IO.IOPort.outb(0x40, (byte)(divisor & 0xFF));
+            IO.IOPort.outb(0x40, (byte)((divisor >> 8) & 0xFF));
+        }
+        public static void Beep(uint frequency)
+        {
+            uint divisor = 1193180 / frequency;
+            IO.IOPort.outb(0x43, 0xB6);
+            IO.IOPort.outb(0x42, (byte)(divisor & 0xFF));
+            IO.IOPort.outb(0x42, (byte)((divisor >> 8) & 0xFF));
+        }
+        internal static bool called = false;
+        public static void SleepSeconds(uint seconds)
+        {
+            SleepMilliseconds(seconds * 1000);
+        }
+        public static void SleepMilliseconds(uint milliseconds)
+        {
+            if (milliseconds <= 50)
+            {
+                called = false;
+                Mode0(milliseconds / 1000);
+                while (!called) { }
+                called = false;
+            }
+            else
+            {
+                uint mod = milliseconds % 100;
+                uint ms = milliseconds - mod;
+                for (int i = 0; i < ms; i += 50)
+                {
+                    called = false;
+                    Mode0(20);
+                    while (!called) { }
+                }
+                called = false;
+                ms = mod % 2;
+                for (int i = 0; i < ms; i += 2)
+                {
+                    called = false;
+                    Mode0(500);
+                    while (!called) { }
+                }
+                called = false;
+            }
+        }
+    }
+}
