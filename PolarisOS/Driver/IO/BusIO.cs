06aa5bf3c2de75a411df98716880ddb077c56e13
Yuuki Wesp <ls-micro@ya.ru>
2016-04-14 11:02:14 +0300

    add bus io

---
+++ PolarisOS/Driver/IO/BusIO.cs
@@ -0,0 +1,16 @@
+﻿namespace PolarisOS.Driver.IO
+{
+    using System;
+
+    public abstract class BusIO
+    {
+        //TODO: Reads and writes can use this to get port instead of argument
+        protected static void Write8(UInt16 aPort, byte aData) { } // Plugged
+        protected static void Write16(UInt16 aPort, UInt16 aData) { } // Plugged
+        protected static void Write32(UInt16 aPort, UInt32 aData) { } // Plugged
+
+        protected static byte Read8(UInt16 aPort) { return 0; } // Plugged
+        protected static UInt16 Read16(UInt16 aPort) { return 0; } // Plugged
+        protected static UInt32 Read32(UInt16 aPort) { return 0; } // Plugged
+    }
+}
