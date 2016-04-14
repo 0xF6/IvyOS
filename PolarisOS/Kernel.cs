9d8c1d1540d4e69b1011eff5a2228f268fdd41c7
Yuuki Wesp <ls-micro@ya.ru>
2016-04-14 12:21:32 +0300

    add kernel

---
+++ PolarisOS/Kernel.cs
@@ -0,0 +1,18 @@
+﻿using Cosmos.System.FileSystem;
+using Cosmos.System.Network;
+
+namespace PolarisOS
+{
+    public class Kernel : Cosmos.System.Kernel
+    {
+        public static string CurrentUser = "";
+        public static bool connected = false;
+        public static UdpClient client = new UdpClient();
+        public static bool nonchoix = false;
+
+        protected override void Run()
+        {
+            
+        }
+    }
+}
