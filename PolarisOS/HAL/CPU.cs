56ca7ef679ef6ded88ffbb288872baf047f9f10b
Yuuki Wesp <ls-micro@ya.ru>
2016-04-16 13:57:08 +0300

    add CPU manager

---
+++ PolarisOS/HAL/CPU.cs
@@ -0,0 +1,13 @@
+﻿namespace PolarisOS.HAL
+{
+    public static class CPU
+    {
+        public static void Reboot() => Cosmos.HAL.Power.Reboot();
+        public static void Wait(uint milliseconds)
+        {
+            Cosmos.HAL.PIT pit = new Cosmos.HAL.PIT();
+            pit.Wait(milliseconds);
+        }
+        public static void WaitSecs(int seconds) => Wait((uint)seconds * 1000);
+    }
+}
