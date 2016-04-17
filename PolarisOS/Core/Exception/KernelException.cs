6278473fa8d40b933a7f0ebe476c469df19d7b4f
Yuuki Wesp <ls-micro@ya.ru>
2016-04-17 15:25:22 +0300

    add IRQ Context data to KernelException

--- PolarisOS/Core/Exception/KernelException.cs
+++ PolarisOS/Core/Exception/KernelException.cs
@@ -4,12 +4,21 @@
     {
         public string MethodBase { private set; get; }
         public ulong UID { private set; get; } = 289275347456;
-
+        public Cosmos.Core.INTs.IRQContext context { private set; get; }
+    
         public KernelException(string msg) : base(msg) { }
 
         public KernelException(string msg, string method) : this(msg)
         {
             MethodBase = method;
+        }
+        public KernelException(string msg, Cosmos.Core.INTs.IRQContext context) : this(msg)
+        {
+            this.context = context;
+        }
+        public KernelException(string msg, Cosmos.Core.INTs.IRQContext context, string method) : this(msg, method)
+        {
+            this.context = context;
         }
     }
 }
